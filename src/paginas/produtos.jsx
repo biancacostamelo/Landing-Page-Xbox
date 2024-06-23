e044ae563ab1f346911b5d3a0b55593910fb19a8
Bianca <bianca.costamelo1@gmail.com>
2024-06-23 16:51:40 -0300

    excluindo cards de produtos

--- src/paginas/produtos.jsx
+++ src/paginas/produtos.jsx
@@ -141,68 +141,6 @@ const Produtos = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="row p-5 meiorow66">
-                        <div className="col-lg-3 card11 meiocol2 tam56 m-2 p-2">
-                            <div className=" p-2 meiocol">
-                                <div className="eclipse">
-                                    <img className="cardimagem p-1" src={controleAzul} alt="Card"></img>
-                                </div>
-                                <div className="container">
-                                    <div class="card-body meiocol3">
-                                        <h5 class="fontebranca mt-3"> <b>Xbox Controle</b></h5>
-                                        <img className="imgavaliacao" src={avaliacao} alt="avaliacao" />
-                                        <h6 class="fontebranca mn">R$100.00</h6>
-                                    </div>
-                                </div>
-                                <button className="botaocarrinho mt-3 fontebranca">Adicionar +</button>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 card11 meiocol2 tam56 m-2 p-2">
-                            <div className=" p-2 meiocol">
-                                <div className="eclipse">
-                                    <img className="cardimagem p-1" src={controle55} alt="Card"></img>
-                                </div>
-                                <div className="container">
-                                    <div class="card-body meiocol3">
-                                        <h5 class="fontebranca mt-3"> <b>Xbox Controle</b></h5>
-                                        <img className="imgavaliacao" src={avaliacao} alt="avaliacao" />
-                                        <h6 class="fontebranca mn">R$100.00</h6>
-                                    </div>
-                                </div>
-                                <button className="botaocarrinho mt-3 fontebranca">Adicionar +</button>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 card11 meiocol2 tam56 m-2 p-2">
-                            <div className=" p-2 meiocol">
-                                <div className="eclipse">
-                                    <img className="cardimagem p-1" src={controleMisto} alt="Card"></img>
-                                </div>
-                                <div className="container">
-                                    <div class="card-body meiocol3">
-                                        <h5 class="fontebranca mt-3"> <b>Xbox Controle</b></h5>
-                                        <img className="imgavaliacao" src={avaliacao} alt="avaliacao" />
-                                        <h6 class="fontebranca mn">R$100.00</h6>
-                                    </div>
-                                </div>
-                                <button className="botaocarrinho mt-3 fontebranca">Adicionar +</button>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 card11 meiocol2 tam56 m-2 p-2">
-                            <div className="p-2 meiocol">
-                                <div className="eclipse">
-                                    <img className="cardimagem p-1" src={controleRoxo} alt="Card"></img>
-                                </div>
-                                <div className="container">
-                                    <div class="card-body meiocol3">
-                                        <h5 class="fontebranca mt-3"> <b>Xbox Controle</b></h5>
-                                        <img className="imgavaliacao" src={avaliacao} alt="avaliacao" />
-                                        <h6 class="fontebranca mn">R$100.00</h6>
-                                    </div>
-                                </div>
-                                <button className="botaocarrinho mt-3 fontebranca">Adicionar +</button>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
